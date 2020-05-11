54cbcb316cc32928df90620186bd44b872fde0f2
jiangtao7913 <6511010602qq.com>
2020-05-11 16:02:15 +0800

    添加springMVC配置文件属性use-default-filters="false" 不使用默认的扫描过滤

--- base-spring/src/main/java/com/jt/controller/BaseController.java
+++ base-spring/src/main/java/com/jt/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.jt.controller;
 
+import com.jt.controller.test.IngoreAnnotation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Author jt
  * @Date 2020/5/6/0006
  **/
+@IngoreAnnotation
 @Controller
 @RequestMapping("/base")
 public class BaseController {
@@ -16,5 +18,10 @@ public class BaseController {
     @RequestMapping("/test")
     public void test(){
         System.out.println("测试springmvc");
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+       return "views/index";
     }
 }
