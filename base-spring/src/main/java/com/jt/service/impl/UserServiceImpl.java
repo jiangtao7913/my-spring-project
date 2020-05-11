77cecfe180e072384409cbae5a9076e15ebd4c66
jiangtao7913 <6511010602qq.com>
2020-05-11 18:22:40 +0800

    druid 添加用户名和密码加密

--- base-spring/src/main/java/com/jt/service/impl/UserServiceImpl.java
+++ base-spring/src/main/java/com/jt/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.jt.service.impl;
 import com.jt.dal.mapper.UserMapper;
 import com.jt.entity.User;
 import com.jt.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import javax.annotation.PostConstruct;
  * @Author jt
  * @Date 2020/4/20/0020
  **/
+@Slf4j
 @Service("userService")
 public class UserServiceImpl implements UserService, InitializingBean {
 
@@ -22,6 +24,7 @@ public class UserServiceImpl implements UserService, InitializingBean {
     private UserMapper userMapper;
     @Override
     public User findUserById() {
+        log.debug("测试日志打印");
         System.out.println("测试1111111111");
         return userMapper.findUserById(1);
     }
